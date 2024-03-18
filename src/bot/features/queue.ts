31b95373a958989170f4d59d63a1960b06225c9e
Vladimir Babin <vovababin@gmail.com>
2024-03-19 00:30:21 +0300

    Add not necessary condition

--- src/bot/features/queue.ts
+++ src/bot/features/queue.ts
@@ -62,7 +62,7 @@ feature.callbackQuery(
           const result = await api.sendMessage(
             // `Write the beginning of the new RPG character's story named "${name}".
             `Write an inspiring text about a person named "${name}" who has decided to start a journey.
-            You can also use this additional information: ${info}. 
+            You could also use this additional information: "${info}", but not required.
             Response MUST BE up to 500 characters maximum`,
           );
           selectedUser.nftDescription = result.text.slice(0, 700);
