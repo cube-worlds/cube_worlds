d1c88850d8261c5a10749072942ad5942edeff8d
Vladimir Babin <vovababin@gmail.com>
2024-03-21 16:59:57 +0500

    Return old prompt if arguement is empty

--- src/bot/features/propmts.ts
+++ src/bot/features/propmts.ts
@@ -9,26 +9,34 @@ const feature = composer.chatType("private").filter(isAdmin);
 
 feature.command("positive", logHandle("command-propmpts"), async (ctx) => {
   const oldPositivePrompt = ctx.dbuser.positivePrompt;
-  ctx.dbuser.positivePrompt = ctx.match;
-  await ctx.dbuser.save();
-  return ctx.reply(
-    `POSITIVE PROMPT WAS UPDATED
-
-Old prompt: <code>${oldPositivePrompt}</code>
-New prompt: <code>${ctx.dbuser.positivePrompt}</code>`,
-  );
+  const newPositivePrompt = ctx.match;
+  if (newPositivePrompt.trim()) {
+    ctx.dbuser.positivePrompt = newPositivePrompt;
+    await ctx.dbuser.save();
+    return ctx.reply(
+      `POSITIVE PROMPT WAS UPDATED
+  
+  Old prompt: <code>${oldPositivePrompt}</code>
+  New prompt: <code>${ctx.dbuser.positivePrompt}</code>`,
+    );
+  }
+  return ctx.reply(`<code>${newPositivePrompt}</code>`);
 });
 
 feature.command("negative", logHandle("command-propmpts"), async (ctx) => {
   const oldNegativePrompt = ctx.dbuser.negativePrompt;
-  ctx.dbuser.negativePrompt = ctx.match;
-  await ctx.dbuser.save();
-  return ctx.reply(
-    `NEGATIVE PROMPT WAS UPDATED
+  const newNegativePrompt = ctx.match;
+  if (newNegativePrompt.trim()) {
+    ctx.dbuser.negativePrompt = newNegativePrompt;
+    await ctx.dbuser.save();
+    return ctx.reply(
+      `NEGATIVE PROMPT WAS UPDATED
       
 Old prompt: <code>${oldNegativePrompt ?? ""}</code>
 New prompt: <code>${ctx.dbuser.negativePrompt}</code>`,
-  );
+    );
+  }
+  return ctx.reply(`<code>${newNegativePrompt}</code>`);
 });
 
 export { composer as promptsFeature };
