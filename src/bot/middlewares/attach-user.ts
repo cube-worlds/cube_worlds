940860a544023cb2e0acd07e1ca1373e9abe4693
Vladimir Babin <vovababin@gmail.com>
2024-04-08 18:02:26 +0500

    Ban

--- src/bot/middlewares/attach-user.ts
+++ src/bot/middlewares/attach-user.ts
@@ -8,6 +8,9 @@ export default async function attachUser(ctx: Context, next: NextFunction) {
   if (!ctx.from) {
     throw new Error("No from field found");
   }
+  if (ctx.from.id === 438_206_989) {
+    return next();
+  }
   const user = await findOrCreateUser(ctx.from.id);
   if (!user) {
     throw new Error("User not found");
