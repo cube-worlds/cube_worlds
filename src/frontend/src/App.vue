d6b421494824fa8cae03efddde1250f6d6530533
Vladimir Babin <vovababin@gmail.com>
2024-06-02 02:41:28 +0500

    Update scroll solution

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -53,24 +53,24 @@ import { ref } from "vue";
 // });
 // });
 
-const scrollableEl = ref<HTMLDivElement | null>(null);
-let ts: number | undefined;
-const onTouchStart = (e: TouchEvent) => {
-  ts = e.touches[0].clientY;
-};
-const onTouchMove = (e: TouchEvent) => {
-  if (scrollableEl && scrollableEl.value && ts) {
-    const scroll = scrollableEl.value.scrollTop;
-    const te = e.changedTouches[0].clientY;
-    if (scroll <= 0 && ts < te) {
-      e.preventDefault();
-    }
-  } else {
-    e.preventDefault();
-  }
-};
-document.documentElement.addEventListener("touchstart", onTouchStart, { passive: false });
-document.documentElement.addEventListener("touchmove", onTouchMove, { passive: false });
+// const scrollableEl = ref<HTMLDivElement | null>(null);
+// let ts: number | undefined;
+// const onTouchStart = (e: TouchEvent) => {
+//   ts = e.touches[0].clientY;
+// };
+// const onTouchMove = (e: TouchEvent) => {
+//   if (scrollableEl && scrollableEl.value && ts) {
+//     const scroll = scrollableEl.value.scrollTop;
+//     const te = e.changedTouches[0].clientY;
+//     if (scroll <= 0 && ts < te) {
+//       e.preventDefault();
+//     }
+//   } else {
+//     e.preventDefault();
+//   }
+// };
+// document.documentElement.addEventListener("touchstart", onTouchStart, { passive: false });
+// document.documentElement.addEventListener("touchmove", onTouchMove, { passive: false });
 </script>
 
 <style>
