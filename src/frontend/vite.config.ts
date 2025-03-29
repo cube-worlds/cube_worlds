a8efbe54eadb5cfad13e43e53877682b60296a93
Vladimir Babin <vovababin@gmail.com>
2025-03-29 14:17:43 +0500

    Add vite config allowedHosts

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -8,6 +8,9 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    allowedHosts: ['cubeworlds.club', 'dominant-annually-lobster.ngrok-free.app'],
+  },
   plugins: [
     vue(),
     AutoImport({
