254a7ada0f235a68f239124ad8369715f346a3cf
Vladimir Babin <vovababin@gmail.com>
2025-12-11 18:56:03 +0500

    Update linter settings to format .vue files

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,23 @@
 import antfu from '@antfu/eslint-config'
+import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
 
 export default antfu({
     typescript: true,
+    vue: true,
     stylistic: {
         indent: 4,
         quotes: 'single',
+        semi: false,
     },
     rules: {
         'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+        'vue/no-unused-vars': 'error',
     },
-})
+    languageOptions: {
+        sourceType: 'module',
+        globals: {
+            ...globals.browser,
+        },
+    },
+}, ...pluginVue.configs['flat/recommended'])
